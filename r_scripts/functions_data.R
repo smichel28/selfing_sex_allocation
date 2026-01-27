2626a6d8dedf118b5e7a2a98eff1fb1e6e44a809
smichel28 <samuel.michel@unil.ch>
2026-01-27 12:37:37 +0100

    includes columns for the generation and the slope/intercept

--- r_scripts/functions_data.R
+++ r_scripts/functions_data.R
@@ -25,16 +25,16 @@ extract_and_saves <- function(files,
                               model = 'simple',
                               nind = 10,
                               nhapl = 10,
-                              save_name,
+                              save.name,
                               read.wd,
                               write.wd) {
+  # extract data from all simulation results for a certain model (i.e. combines the results for different parameter values) and write results' summary
   
-  means_ind <- paste(write.wd, paste0(save_name, '_means_ind.tsv'), sep = '/')
-  means_hapl <-  paste(write.wd, paste0(save_name, '_means_hapl.tsv'), sep = '/')
-  
-  sampled_ind <-  paste(write.wd, paste0(save_name, '_sampled_ind.tsv'), sep = '/')
-  sampled_hapl <-  paste(write.wd, paste0(save_name, '_sampled_hapl.tsv'), sep = '/')
-  
+  # creates files where summary data will be written
+  means_ind <- paste(write.wd, paste0(save.name, '_means_ind.tsv'), sep = '/')
+  means_hapl <-  paste(write.wd, paste0(save.name, '_means_hapl.tsv'), sep = '/')
+  sampled_ind <-  paste(write.wd, paste0(save.name, '_sampled_ind.tsv'), sep = '/')
+  sampled_hapl <-  paste(write.wd, paste0(save.name, '_sampled_hapl.tsv'), sep = '/')
   file.create(means_ind)
   file.create(means_hapl)
   file.create(sampled_ind)
@@ -51,17 +51,16 @@ extract_and_saves <- function(files,
     data <- as.data.frame(fread(paste(read.wd, f, sep = '/'), header = TRUE))
     selected_generations <- data[['generation']] %% every.gen == 0 | data[['generation']] == 10
     data <- data[selected_generations, ]
-
+    
     n_row <- nrow(data)
     n_param <- length(info[['columns']])
     
+    # creates a dataframe containing the parameter of the simulation of the current file
     parameters_simulation <- NULL
     for (i in 1:n_param) {
       
       col_name <- info[['columns']][i]
       value <- info[['parameter_values']][i]
-      
-     # if (col_name == 'nsampled') {n_sampled <- as.integer(value)}
       
       if (is.null(parameters_simulation)) {
         parameters_simulation <- matrix(rep(value, times = n_row))
@@ -73,15 +72,17 @@ extract_and_saves <- function(files,
     colnames(parameters_simulation) <- info[['columns']]
     parameters_simulation <- as.data.frame(parameters_simulation)
     
-    n_sampled <- as.integer(info[['parameter_values']][info[['columns']] == 'nsampled'])
+    n_sampled <- as.integer(info[['parameter_values']][info[['columns']] == 'nsampled']) # number of individuals sampled in the simulation every 1000 generation
     
     # creates two datasets, one contains phenotypes and the other contains allele values
     data_ind <- cbind(parameters_simulation, data[,1:(n_sampled+2)])
     data_hapl <- cbind(parameters_simulation, data[,c(1,2,(n_sampled+3):(n_sampled*2+2))])
-
+    
+    
+    
     # saves the mean phenotype and allele every desired generation
     write.table(
-      cbind(parameters_simulation, data.frame(means = rowMeans(data_ind[,-c(1:(n_param+2))]))), ## ATTENTION CA SÉLECTIONNE PAS BIEN (IL MANGE LES GÉNÉRATIONS ET SLOPE/INTERCEPT)
+      cbind(data_ind[,1:(n_param+2)], data.frame(means = rowMeans(data_ind[,-c(1:(n_param+2))]))),
       file = means_ind,
       sep = "\t",
       row.names = FALSE,
@@ -89,14 +90,14 @@ extract_and_saves <- function(files,
       append = TRUE
     )
     write.table(
-      cbind(parameters_simulation, data.frame(means = rowMeans(data_hapl[,-c(1:(n_param+2))]))),
+      cbind(data_hapl[,1:(n_param+2)], data.frame(means = rowMeans(data_hapl[,-c(1:(n_param+2))]))),
       file = means_hapl,
       sep = "\t",
       row.names = FALSE,
       col.names = write_col,
       append = TRUE
     )
-
+    
     # saves a few phenotypes or alleles every desired generation
     write.table(
       data_ind[, 1:(n_param+2+nind)] %>% 
